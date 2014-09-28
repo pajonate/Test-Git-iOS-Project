4a991179e9f6cc0ff8d324422243493e707e5317
pajonate <pajo.nate@gmail.com>
2014-09-28 01:23:33 -0400

    added a global test class

---
+++ Git_Test/PA_Global_test.h
@@ -0,0 +1,13 @@
+//
+//  PA_Global_test.h
+//  Git_Test
+//
+//  Created by Pavle on 9/28/14.
+//  Copyright (c) 2014 Pavle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PA_Global_test : NSObject
+
+@end
---
+++ Git_Test/PA_Global_test.m
@@ -0,0 +1,13 @@
+//
+//  PA_Global_test.m
+//  Git_Test
+//
+//  Created by Pavle on 9/28/14.
+//  Copyright (c) 2014 Pavle. All rights reserved.
+//
+
+#import "PA_Global_test.h"
+
+@implementation PA_Global_test
+
+@end
