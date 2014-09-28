67c7671d5cbfb2b2d13bf00f94d50ab70d9a16a7
pajonate <pajo.nate@gmail.com>
2014-09-28 01:19:54 -0400

    added a test class

---
+++ Git_Test/PA_Test.h
@@ -0,0 +1,13 @@
+//
+//  PA_Test.h
+//  Git_Test
+//
+//  Created by Pavle on 9/28/14.
+//  Copyright (c) 2014 Pavle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PA_Test : NSObject
+
+@end
---
+++ Git_Test/PA_Test.m
@@ -0,0 +1,13 @@
+//
+//  PA_Test.m
+//  Git_Test
+//
+//  Created by Pavle on 9/28/14.
+//  Copyright (c) 2014 Pavle. All rights reserved.
+//
+
+#import "PA_Test.h"
+
+@implementation PA_Test
+
+@end
